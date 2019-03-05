6283081bb79faac85edd0886de9b5a79928134c5
Olivier Berni <olivier@lunabee.com>
2019-03-05 16:34:51 +0100

    QR Code redirection HTML

--- qrcode.php
+++ qrcode.php
@@ -1,2 +1,16 @@
-header("Location: http://www.86percent.co"); /* Redirect browser */
-exit();
+<!DOCTYPE HTML>
+<html lang="en-US">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="refresh" content="0; url=http://www.86percent.co">
+        <script type="text/javascript">
+            window.location.href = "http://www.86percent.co"
+        </script>
+        <title>86% SDK ... hold on for 2s.</title>
+    </head>
+    <body>
+        <!-- Note: don't tell people to `click` the link, just tell them that it is a link. -->
+        If you are not redirected automatically, follow this <a href='http://www.86percent.co'>link to example</a>.
+    </body>
+</html>
+
